4523f7ca46a2a7c0022a73a08964b0b2639ce912
coder-repeat <coderepeat44@gmail.com>
2020-07-13 17:12:03 -0300

    Calcule seu IMC v1

--- imc/views.py
+++ imc/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def imc(request):
+    imc_result = ''
+    if request.GET.get('height') and request.GET.get('weight') != '':
+        height = float(request.GET.get('height'))
+        weight = float(request.GET.get('weight'))
+        imc_result = weight / height ** 2
+
+
+    return render(request, 'imc/imcapp.html', {'imc':round(imc_result, 2)})
